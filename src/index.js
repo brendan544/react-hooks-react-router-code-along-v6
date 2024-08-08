a01df021ceb7e1dbefd890a64cf6a1872b6a3e91
Brendan Gwer <brendan.gwer@student.moringaschool.com>
2024-08-08 11:02:37 +0300

    added more routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
+import Login from "./pages/Login";
 
 
 const router = createBrowserRouter([
@@ -10,7 +11,14 @@ const router = createBrowserRouter([
         path: "/" ,
         element: <Home />
     }
-
+    {
+        path: "/About" ,
+         element: <About/>
+    } 
+    {
+        path: "/Login" ,
+         element: <login/>
+    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
